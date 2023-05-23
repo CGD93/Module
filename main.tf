b278591f936e61904838578dc02899def82d54a7
ChoiGwangDeok <111724209+CGD93@users.noreply.github.com>
2023-05-23 10:57:18 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ resource "azurerm_virtual_network" "VN" {
   name                  = var.VN-name 
   resource_group_name   = azurerm_resource_group.RG.name
   location              = azurerm_resource_group.RG.location
-  address_space         = var.VN-address
+  address_space         = [var.VN-address]
 }
 
 resource "azurerm_subnet" "Subnet" {
   name                  = var.sub-name
   resource_group_name   = azurerm_resource_group.RG.name
   virtual_network_name  = azurerm_virtual_network.VN.name
-  address_prefixes      = var.sub-add
+  address_prefixes      = [var.sub-add]
 }
