6a7dff94aec1fa4fbc2466f2b18bfd9589d22f9c
ChoiGwangDeok <111724209+CGD93@users.noreply.github.com>
2023-06-09 09:32:54 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,23 @@
 resource "azurerm_resource_group" "RG" {
-  name          = var.RG-name
+  name          = var.RG-name[count.index]
   location      = var.RG-location
+  count = var.coun
   tags          = var.tag
 }
 
 resource "azurerm_virtual_network" "VN" {
-  name                  = var.VN-name 
-  resource_group_name   = azurerm_resource_group.RG.name
-  location              = azurerm_resource_group.RG.location
-  address_space         = [var.VN-address]
+  name                  = var.VN-name[count.index]
+  resource_group_name   = azurerm_resource_group.RG[count.index].name
+  location              = azurerm_resource_group.RG[count.index].location
+  count = var.coun
+  address_space         = [var.VN-address[count.index]]
   tags          = var.tag
 }
 
 resource "azurerm_subnet" "Subnet" {
-  name                  = var.sub-name
-  resource_group_name   = azurerm_resource_group.RG.name
-  virtual_network_name  = azurerm_virtual_network.VN.name
-  address_prefixes      = [var.sub-add]
+  name                  = var.sub-name[count.index]
+  resource_group_name   = azurerm_resource_group.RG[count.index].name
+  virtual_network_name  = azurerm_virtual_network.VN[count.index].name
+  count = var.coun
+  address_prefixes      = [var.sub-add[count.index]]
 }
